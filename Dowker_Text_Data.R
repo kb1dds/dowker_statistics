5c47eb6ba7116339644c87c2d9514ee0390c8e62
Jaehee Lee <jaeheelee1217@gmail.com>
2023-03-26 23:34:22 -0400

    Updated
    Created functions to remove duplicates. Saved topics and gutenberg id csv file in the github

--- Dowker_Text_Data.R
+++ Dowker_Text_Data.R
@@ -1,6 +1,5 @@
-
-## List Dependencies
-## Make sure to download ....
+# Dependencies 
+# Please download gutenbergr, tidyverse, tm, tidytext, ggthemes, gridExtra to use this package 
 
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ##
@@ -9,9 +8,8 @@
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
 library(pacman)
-
 ## Loading required packages and install them if not yet installed 
-p_load(gutenbergr, tidyverse, tm, tidytext, ggthemes)
+p_load(gutenbergr, tidyverse, tm, tidytext, ggthemes, gridExtra)
 
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ##
@@ -21,36 +19,8 @@ p_load(gutenbergr, tidyverse, tm, tidytext, ggthemes)
 
 # Use Gutenberg book data
 gutenberg_metadata <- gutenberg_metadata
-
-# save as csv file 
-# "gutenberg_download" function is to download works using a project Gutenberg ID.
-# Use gutenberg text data topics in Politics, Art, Biology, Cookery, Travel and combine them together:
-gutenberg_15books <- gutenberg_download(c(5669, 147, 17306, 4776, 3207, 20125, 35894, 9596, 1341, 151, # Politics 10 
-                                          7370, 612, 18, 2130, 6762, 1232, 20433, 39622, 34111, 15509, # Politics 20
-                                          34645, 2176, 5000, 11242, 17408, 38532, 45504, 29904, 2398, 17373, # Art 10
-                                          167, 20195, 3226, 5321, 3751, 5712, 13119, 16180, 17244, 22564, # Art 20
-                                          1909, 2089, 2927, 20818, 18911, 6329, 14473, 27778, 2009, 4962, # Biology 10
-                                          17966, 6052, 13249, 23755, 18350, 22764, 2938, 16410, 18884, 24456, # Biology 20 
-                                          10011, 24384, 13265, 25631, 15019, 26032, 12815, 24205, 33974, 29730, # Cookery 10
-                                          24542, 25007, 31534, 27245, 6978, 6429, 26558, 29329, 6385, 34822, # Cookery 20
-                                          944, 1082, 3177, 3704, 6317, 39496, 12422, 15777, 22117, 28323, # Travel 10
-                                          39806, 39685, 39615, 39474, 39308, 39082, 39026, 38203, 27250, 22409), # Travel 20 
-                                        mirror = "http://mirrors.xmission.com/gutenberg/")
-
-## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-##
-## 1.2 - Documents, Topics (2 columns)                                                                                ----
-##
-## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
-data <- gutenberg_15books %>% 
-  mutate(Topics = case_when(
-  gutenberg_id %in% c(5669, 147, 17306, 4776, 3207, 20125, 35894, 9596, 1341, 151, 7370, 612, 18, 2130, 6762, 1232, 20433, 39622, 34111, 15509) ~ "Politics",
-  gutenberg_id %in% c(34645, 2176, 5000, 11242, 17408, 38532, 45504, 29904, 2398, 17373, 167, 20195, 3226, 5321, 3751, 5712, 13119, 16180, 17244, 22564) ~ "Art",
-  gutenberg_id %in% c(1909, 2089, 2927, 20818, 18911, 6329, 14473, 27778, 2009, 4962, 17966, 6052, 13249, 23755, 18350, 22764, 2938, 16410, 18884, 24456) ~ "Biology",
-  gutenberg_id %in% c(10011, 24384, 13265, 25631, 15019, 26032, 12815, 24205, 33974, 29730, 24542, 25007, 31534, 27245, 6978, 6429, 26558, 29329, 6385, 34822) ~ "Cookery", 
-  gutenberg_id %in% c(944, 1082, 3177, 3704, 6317, 39496, 12422, 15777, 22117, 28323,39806, 39685, 39615, 39474, 39308, 39082, 39026, 38203, 27250, 22409) ~ "Travel", 
-  TRUE ~ NA_character_ # for cases not covered by any of the above conditions
-))
+read.csv("gutenberg_sample_data.csv", header = TRUE, sep = ",") -> id_topics
+gutenberg_15books <- gutenberg_download(c(id_topics$gutenberg_id), mirror = "http://mirrors.xmission.com/gutenberg/")
 
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ##
@@ -58,7 +28,7 @@ data <- gutenberg_15books %>%
 ##
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-book_words <- data %>%  
+book_words <- left_join(gutenberg_15books, id_topics, by = "gutenberg_id") %>%  
   unnest_tokens(word, text) %>%  
   count(Topics, gutenberg_id, word, sort = TRUE) %>%  
   ungroup()
@@ -88,7 +58,7 @@ stop_words <- removeNumbers(stop_words_tf_idf$word) # remove numbers
 ##
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-data %>% 
+left_join(gutenberg_15books, id_topics, by = "gutenberg_id") %>% 
   group_by(gutenberg_id, Topics) %>% 
   slice_sample(n=500, replace = TRUE) %>% 
   unnest_tokens(word, text) %>%  
@@ -168,16 +138,13 @@ dowker_nest <- function(df,feature_vars,obs_vars){
 
 #### Join the topics to the books_nonzero data
 
-### Change later 
-new_df
-
 # Apply Dowker Complex function to books_nonzero 
-# books_nonzero %>%  
-#   as_tibble() %>%  
-#   dowker_nest(Docs, Terms) -> books_dowker_nest
+books_nonzero %>%
+  as_tibble() %>%
+  dowker_nest(Docs, Terms) -> books_dowker_nest
 
 ### change later
-dowker_nest(new_df,c(Docs, Topics), Terms) -> books_dowker_nest
+# dowker_nest(books_nonzero,c(Docs, Topics), Terms) -> books_dowker_nest
 
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ##
@@ -248,78 +215,32 @@ books_dowker_nest %>%
   mutate(score = weight*feature_count) -> books_dowker_nest
 
 # art words
-books_dowker_nest %>%  
-  filter(prob_art > 0.9) %>%  
-  select(observations) %>%  
-  unnest(observations) %>%  
-  left_join(books_nonzero %>%  
-              as_tibble(rownames = "Word")) %>%  
-  select("Word") %>%  
-  unique() -> art_words
 
-# biology words
-books_dowker_nest %>%  
-  filter(prob_biology > 0.9) %>%  
-  select(observations) %>%  
-  unnest(observations) %>%  
-  left_join(books_nonzero %>%  
-              as_tibble(rownames = "Word")) %>%  
-  select("Word") %>%  
-  unique() -> biology_words
+find_target_words <- function(data, target_col, threshold) {
+  words <- data %>%
+    filter({{ target_col }} > threshold) %>%
+    select(observations) %>%
+    unnest(observations) %>%
+    left_join(books_nonzero %>% as_tibble(rownames = "Word")) %>%
+    select("Word") %>%
+    unique()
+  return(words)
+}
 
-# politics words
-books_dowker_nest %>%  
-  filter(prob_politics > 0.9) %>%  
-  select(observations) %>%  
-  unnest(observations) %>%  
-  left_join(books_nonzero %>%  
-              as_tibble(rownames = "Word")) %>%  
-  select("Word") %>%  
-  unique() -> politics_words
+# usage example: find art words with probability greater than 0.9
+art_words <- find_target_words(books_dowker_nest, prob_art, 0.9)
 
-# cookery words
-books_dowker_nest %>%  
-  filter(prob_cookery > 0.9) %>%  
-  select(observations) %>%  
-  unnest(observations) %>%  
-  left_join(books_nonzero %>%  
-              as_tibble(rownames = "Word")) %>%  
-  select("Word") %>%  
-  unique() -> cooker_words
+# usage example: find biology words with probability greater than 0.9
+biology_words <- find_target_words(books_dowker_nest, prob_biology, 0.9)
 
-# politics words
-books_dowker_nest %>%  
-  filter(prob_travel > 0.9) %>%  
-  select(observations) %>%  
-  unnest(observations) %>%  
-  left_join(books_nonzero %>%  
-              as_tibble(rownames = "Word")) %>%  
-  select("Word") %>%  
-  unique() -> travel_words
+# usage example: find politics words with probability greater than 0.9
+politics_words <- find_target_words(books_dowker_nest, prob_politics, 0.9)
 
-# make new column called topics in order to use it later
-logical_full_books%>%  
-  filter(politics %in% TRUE) %>%  
-  mutate(topics = "politics") -> politics_data
-logical_full_books %>%  
-  filter(art %in% TRUE) %>%  
-  mutate(topics = "art") -> art_data
-logical_full_books %>%  
-  filter(biology %in% TRUE) %>%  
-  mutate(topics = "biology") -> biology_data
-logical_full_books %>%  
-  filter(cookery %in% TRUE) %>%  
-  mutate(topics = "cookery") -> cookery_data
-logical_full_books %>%  
-  filter(politics %in% FALSE & art %in% FALSE & biology %in% FALSE & cookery %in% FALSE) %>%  
-  mutate(topics = "travel") -> travel_data
+# usage example: find cookery words with probability greater than 0.9
+cooker_words <- find_target_words(books_dowker_nest, prob_cookery, 0.9)
 
-# combine three data into one 
-politics_data %>%  
-  rbind(art_data) %>%  
-  rbind(biology_data) %>%  
-  rbind(cookery_data) %>% 
-  rbind(travel_data) -> complete_data
+# usage example: find travel words with probability greater than 0.9
+travel_words <- find_target_words(books_dowker_nest, prob_travel, 0.9)
 
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ##
@@ -329,10 +250,10 @@ politics_data %>%
 
 books_dowker_nest %>%  
   select(c("observations", "weight", "difference", "score", "feature_count", "feature_pattern", "prob_art", "prob_politics", "prob_biology", "prob_cookery", "prob_travel")) %>%  
-  unnest(feature_pattern) %>%
-  left_join(complete_data %>% mutate(Docs = row_number()),
+  unnest(feature_pattern)  %>%
+  left_join(id_topics %>% mutate(Docs = row_number()),
             by = c(Docs = "Docs")
-  ) -> use_this_data
+  )-> use_this_data
 
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ##
@@ -340,91 +261,24 @@ books_dowker_nest %>%
 ##
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 
-ggplot(use_this_data, aes(x = prob_art)) +
-  geom_histogram()
+# Define function for creating boxplots
+create_boxplot <- function(data, x_var) {
+  ggplot(data, aes(x = {{x_var}}, y = as.factor(Topics))) + 
+    geom_boxplot() +
+    coord_flip() +
+    theme_bw() + 
+    ylab("Topics")
+}
 
-ggplot(use_this_data, aes(x = prob_biology)) +
-  geom_histogram()
+create_boxplot(use_this_data, prob_art) -> prob_art_box_plot
+create_boxplot(use_this_data, prob_biology) -> prob_biology_box_plot
+create_boxplot(use_this_data, prob_politics) -> prob_politics_box_plot
+create_boxplot(use_this_data, prob_cookery) -> prob_cookery_box_plot
+create_boxplot(use_this_data, prob_travel) -> prob_travel_box_plot
 
-ggplot(use_this_data, aes(x = prob_politics)) +
-  geom_histogram()
-
-ggplot(use_this_data, aes(x = prob_cookery)) +
-  geom_histogram()
-
-ggplot(use_this_data, aes(x = prob_travel)) +
-  geom_histogram()
-
-# weight vs feature count 
-ggplot(use_this_data, aes(x = weight, y = feature_count)) + 
-  geom_point() + 
-  theme_bw()
-
-# Scatterplot (score vs difference) -> see the trend, explore, outliers, what they mean??? 
-ggplot(use_this_data, aes(x = difference, y = score, color = weight, size = feature_count)) + 
-  geom_point() + 
-  theme_bw()
-
-# Scatterplot (score vs difference) 
-ggplot(use_this_data, aes(x = difference, y = score)) + 
-  geom_point() + 
-  theme_bw()
-
-# "Weight" vs "Each topic probability"
-ggplot(use_this_data, aes(y=weight, x=prob_art))+
-  geom_point()+ 
-  theme_bw()
-
-ggplot(use_this_data, aes(y=weight, x=prob_biology))+
-  geom_point()+
-  theme_bw()
-
-ggplot(use_this_data, aes(y=weight, x=prob_politics))+
-  geom_point()+
-  theme_bw()
-
-ggplot(use_this_data, aes(y=weight, x=prob_cookery))+
-  geom_point()+
-  theme_bw()
-
-ggplot(use_this_data, aes(y=weight, x=prob_travel))+
-  geom_point()+
-  theme_bw()
-
-# Boxplot (prob art VS topics)
-use_this_data %>%  
-  ggplot(aes(x = prob_art, y = as.factor(topics))) + 
-  geom_boxplot() + 
-  coord_flip()+ 
-  theme_bw() 
-
-# Boxplot (prob biology VS topics)
-use_this_data %>% 
-  ggplot(aes(x = prob_biology, y = as.factor(topics))) + 
-  geom_boxplot() + 
-  coord_flip()+
-  theme_bw()
-
-# Boxplot (prob politics VS topics)
-use_this_data %>% 
-  ggplot(aes(x = prob_politics, y = as.factor(topics))) + 
-  geom_boxplot() + 
-  coord_flip() +
-  theme_bw()
-
-# Boxplot (prob cookery VS topics)
-use_this_data %>% 
-  ggplot(aes(x = prob_cookery, y = as.factor(topics))) + 
-  geom_boxplot() + 
-  coord_flip() +
-  theme_bw()
-
-# Boxplot (prob travel VS topics)
-use_this_data %>% 
-  ggplot(aes(x = prob_travel, y = as.factor(topics))) + 
-  geom_boxplot() + 
-  coord_flip() +
-  theme_bw()
+grid.arrange(prob_art_box_plot, prob_biology_box_plot, 
+             prob_politics_box_plot, prob_cookery_box_plot, prob_travel_box_plot, 
+             nrow = 2)
 
 
 # Interpretation: 
@@ -450,65 +304,6 @@ summary(aov_test_cookery)
 aov_test_travel <- aov(prob_travel ~ as.factor(topics), use_this_data)
 summary(aov_test_travel)
 
-
-###### tf-idf VS Dowker Probability ##### 
-use_this_data %>%  
-  unnest(observations) %>%  
-  left_join(books_nonzero %>%  
-              as_tibble(rownames = "Words")) %>%  
-  left_join(book_words,by = c(Words = "word"))  %>%  
-  ggplot(aes(x =tf_idf, y = prob_art)) + 
-  geom_point()+
-  theme_bw()
-
-# histogram of prob of each one of these topics 
-use_this_data %>%  
-  filter(topics == "politics") %>% 
-  ggplot(aes(x = prob_art)) + 
-  geom_histogram() + 
-  theme_bw()
-
-use_this_data %>%  
-  filter(topics == "politics") %>% 
-  ggplot(aes(x = prob_biology)) + 
-  geom_histogram() + 
-  theme_bw()
-
-use_this_data %>%  
-  filter(topics == "politics") %>% 
-  ggplot(aes(x = prob_cookery)) + 
-  geom_histogram() + 
-  theme_bw()
-
-use_this_data %>%  
-  filter(topics == "politics") %>% 
-  ggplot(aes(x = prob_travel)) + 
-  geom_histogram() + 
-  theme_bw()
-
-use_this_data %>%  
-  filter(topics == "art") %>% 
-  ggplot(aes(x = prob_politics)) + 
-  geom_histogram() + 
-  theme_bw()
-
-use_this_data %>%  
-  filter(topics == "art") %>% 
-  ggplot(aes(x = prob_biology)) + 
-  geom_histogram() + 
-  theme_bw()
-
-use_this_data %>%  
-  filter(topics == "art") %>% 
-  ggplot(aes(x = prob_cookery)) + 
-  geom_histogram() + 
-  theme_bw()
-
-use_this_data %>%  
-  filter(topics == "art") %>% 
-  ggplot(aes(x = prob_travel)) + 
-  geom_histogram() + 
-  theme_bw()
 
 ## +++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
 ##
@@ -540,7 +335,7 @@ dowker_prob <- function(df,class_var){
     ungroup()
 }
 
-dowker_prob(books_dowker_nest, topics) -> df_df
+dowker_prob(books_dowker_nest, Topics) -> dowker_prob_result
 
 # dowker_graph in dowker split
 
